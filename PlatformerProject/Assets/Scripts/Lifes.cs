4d580845972b646d616a008e6b4f470598c2cbbb
Johanna Arponen <johanna.arponen@icloud.com>
2019-11-25 12:41:54 +0200

    Kommit

--- PlatformerProject/Assets/Scripts/Lifes.cs
+++ PlatformerProject/Assets/Scripts/Lifes.cs
@@ -8,6 +8,10 @@ public class Lifes : MonoBehaviour{
     [HideInInspector]
     public float startHealth = 100;
     private float health;
+    public GameObject healthElement;
+    public Canvas canvas;
+    public float gap = 5;
+    private List<GameObject> elements = new List<GameObject>();
 
     public GameObject deathEffect;
 
@@ -23,5 +27,23 @@ public class Lifes : MonoBehaviour{
         if(health <= 0) {
             Die();
         }
+    }
+    void UpdateHealth() {
+        foreach(var element in elements) {
+            Destroy(element);
+        }
+        elements.Clear();
+
+        for(int i = 0; i < health; i++) {
+            var element = Instantiate(healthElement, healthElement.transform.position, healthElement.transform.rotation);
+            element.transform.SetParent(canvas.transform, false);
+
+            var rect = element.GetComponent<RectTransform>();
+            rect.anchoredPosition -= new Vector2(i * (gap + rect.sizeDelta.x), 0);
+            elements.Add(element);
+        }
+    }
+    void Die() {
+        print("Die!!");
     }
 }
