840f5a254244c173b2d8f5ce4e23700019599f9a
Johanna Arponen <johanna.arponen@icloud.com>
2019-12-02 14:33:52 +0200

    Options menu viela

--- PlatformerProject/Assets/Scripts/OptionsMenu.cs
+++ PlatformerProject/Assets/Scripts/OptionsMenu.cs
@@ -2,14 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour{
 
     public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start() {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        for(int i = 0; i< resolutions.Length; i++) {
+            string options = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(options);
+        }
+        resolutionDropdown.AddOptions(options);
+    }
 
     public void SetVolume (float volume) {
         audioMixer.SetFloat("volume", volume);
         Debug.Log(volume);
+    }
+
+    public void SetQuality (int qualityIndex) {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen) {
+        Screen.fullScreen = isFullscreen;
     }
     
 }
