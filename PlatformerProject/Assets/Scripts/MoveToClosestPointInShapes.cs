a0e9cf496d84d3d6fb375a8d06c85286d2628065
Samuel <32210043+Satsaa@users.noreply.github.com>
2019-12-04 12:02:23 +0200

    Editor for lines
    kinda buggy

--- PlatformerProject/Assets/Scripts/MoveToClosestPointInShapes.cs
+++ PlatformerProject/Assets/Scripts/MoveToClosestPointInShapes.cs
@@ -4,42 +4,21 @@ using UnityEngine;
 
 public class MoveToClosestPointInShapes : MonoBehaviour {
 
-  [Tooltip("Contains an ordered list of GameObjects where every first GameObject is the line start and every second is the line end")]
-  public GameObject lineContainer;
   [Tooltip("Calculate based on this transform instead of this.transform")]
   public Transform target;
-  [Tooltip("Delete gameObjects at start and save their positions as vectors only")]
-  public bool deleteGameObjects = true;
   public Vector3 offset = Vector3.zero;
   public bool smoothDamp = true;
   public float dampSpeed = 1;
   public float dampMaxSpeed = 1;
 
-  private Line[] lines = new Line[0];
+  public List<Vector3> lines = new List<Vector3>();
+
   private Vector3 velocity = Vector3.zero;
 
 
   // Start is called before the first frame update
   void Start() {
     if (target == null) target = transform;
-    if (deleteGameObjects) {
-      var list = new List<Line> { };
-
-      var flip = true;
-      Vector2 child1 = Vector2.zero;
-      Vector2 child2;
-      foreach (Transform child in lineContainer.transform) {
-        flip = !flip;
-        if (flip) {
-          child2 = child.position;
-          list.Add(new Line(child1, child2));
-        } else {
-          child1 = child.position;
-        }
-        GameObject.Destroy(child.gameObject);
-      }
-      lines = list.ToArray();
-    }
   }
 
   // Update is called once per frame
@@ -47,80 +26,57 @@ public class MoveToClosestPointInShapes : MonoBehaviour {
     var minVector = Vector2.zero;
     var minVectorLength = float.PositiveInfinity;
 
-    var pos = target.position + offset;
-    if (lines.Length > 0) {
-      foreach (var line in lines) {
-        var dir = line.a - line.b;
-        var res = ClosestPointOnLine(line.a, line.b, pos);
+    var pos = target.position;
+
+    var flip = true;
+    Vector3 pos1 = Vector3.zero; // Value only for initialization
+    Vector3 pos2;
+    foreach (var point in lines) {
+      flip = !flip;
+      if (flip) {
+        pos2 = point;
+        var dir = pos1 - pos2;
+        var res = ClosestPointOnLine(pos1, pos2, pos);
         if (minVectorLength > (res - pos.xy()).sqrMagnitude) {
           minVector = res - pos.xy();
           minVectorLength = minVector.sqrMagnitude;
         }
-      }
-    } else {
-      var flip = true;
-      Transform child1 = transform; // Value only for initialization
-      Transform child2;
-      foreach (Transform child in lineContainer.transform) {
-        flip = !flip;
-        if (flip) {
-          child2 = child;
-          var dir = child1.transform.position - child2.transform.position;
-          var res = ClosestPointOnLine(child1.transform.position, child2.transform.position, pos);
-          if (minVectorLength > (res - pos.xy()).sqrMagnitude) {
-            minVector = res - pos.xy();
-            minVectorLength = minVector.sqrMagnitude;
-          }
-        } else {
-          child1 = child;
-        }
+      } else {
+        pos1 = point;
       }
     }
+
     if (smoothDamp) {
-      transform.position = Vector3.SmoothDamp(transform.position, pos + minVector.xyo(), ref velocity, dampSpeed, dampMaxSpeed, Time.deltaTime);
-    } else transform.position = pos + minVector.xyo();
+      transform.position = Vector3.SmoothDamp(transform.position, pos + minVector.xyo() + offset, ref velocity, dampSpeed, dampMaxSpeed, Time.deltaTime);
+    } else transform.position = pos + minVector.xyo() + offset;
   }
 
   void OnDrawGizmos() {
     var minVector = Vector2.zero;
     var minVectorLength = float.PositiveInfinity;
 
-    var pos = target.position + offset;
-    if (lineContainer != null) {
-      if (lines.Length > 0) {
-        foreach (var line in lines) {
-          var dir = line.a - line.b;
-          var res = ClosestPointOnLine(line.a, line.b, pos);
-          Gizmos.DrawLine(line.a, line.b);
-          MyUtil.DrawCross(res.xyo(), 0.1f, Color.gray);
-          if (minVectorLength > (res - pos.xy()).sqrMagnitude) {
-            minVector = res - pos.xy();
-            minVectorLength = minVector.sqrMagnitude;
-          }
+    var pos = target == null ? transform.position : target.position;
+    var flip = true;
+    Vector3 pos1 = Vector3.zero; // Value only for initialization
+    Vector3 pos2;
+    foreach (var point in lines) {
+      flip = !flip;
+      if (flip) {
+        pos2 = point;
+        var dir = pos1 - pos2;
+        var res = ClosestPointOnLine(pos1, pos2, pos);
+        Gizmos.color = new Color(0.85f, 0.85f, 0.85f, 1);
+        Gizmos.DrawSphere(res.xyo(), 0.1f);
+        if (minVectorLength > (res - pos.xy()).sqrMagnitude) {
+          minVector = res - pos.xy();
+          minVectorLength = minVector.sqrMagnitude;
         }
       } else {
-        var flip = true;
-        Transform child1 = transform; // Value only for initialization
-        Transform child2;
-        foreach (Transform child in lineContainer.transform) {
-          flip = !flip;
-          if (flip) {
-            child2 = child;
-            var dir = child1.transform.position - child2.transform.position;
-            var res = ClosestPointOnLine(child1.transform.position, child2.transform.position, pos);
-            Gizmos.DrawLine(child1.transform.position, child2.transform.position);
-            MyUtil.DrawCross(res.xyo(), 0.1f, Color.gray);
-            if (minVectorLength > (res - pos.xy()).sqrMagnitude) {
-              minVector = res - pos.xy();
-              minVectorLength = minVector.sqrMagnitude;
-            }
-          } else {
-            child1 = child;
-          }
-        }
+        pos1 = point;
       }
     }
-    MyUtil.DrawCross(pos.AddXY(minVector).xyo(), 0.2f, Color.green);
+    Gizmos.color = Color.green;
+    Gizmos.DrawSphere(pos.AddXY(minVector).xyo(), 0.2f);
   }
 
   public static Vector2 ClosestPointOnLine(Vector2 A, Vector2 B, Vector2 P) {
@@ -133,13 +89,5 @@ public class MoveToClosestPointInShapes : MonoBehaviour {
 
     if (distance < 0) return A;
     return (distance > 1 ? B : A + AB * distance);
-  }
-
-  struct Line {
-    public Line(Vector3 a, Vector3 b) {
-      this.a = a; this.b = b;
-    }
-    public Vector3 a;
-    public Vector3 b;
   }
 }
---
+++ PlatformerProject/Assets/Scripts/MoveToClosestPointInShapesCustomEditor.cs
@@ -0,0 +1,92 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MoveToClosestPointInShapes))]
+public class MoveToClosestPointInShapesCustomEditor : Editor {
+
+  private bool mouse = false;
+  private bool blockCreate = false;
+  private int createdAt;
+  private MoveToClosestPointInShapes t { get => ((MoveToClosestPointInShapes)target); }
+
+  protected virtual void OnSceneGUI() {
+    var shift = Event.current.shift;
+    var control = Event.current.control;
+
+
+    switch (Event.current.type) {
+      case EventType.Repaint:
+        Handles.color = Handles.xAxisColor;
+        if (t.lines.Count % 2 != 0) {
+          var tail = t.lines[t.lines.Count - 1];
+          t.lines.RemoveAt(t.lines.Count - 1);
+          Handles.DrawLines(t.lines.ToArray());
+          t.lines.Add(tail);
+        } else
+          Handles.DrawLines(t.lines.ToArray());
+        break;
+      case EventType.MouseDown:
+        if (Event.current.button == 0) {
+          Debug.Log("Left-Mouse Down");
+          mouse = true;
+        }
+        break;
+      case EventType.MouseUp:
+        if (Event.current.button == 0) {
+          Debug.Log("Left-Mouse Down");
+          mouse = false;
+          blockCreate = false;
+        }
+        break;
+    }
+
+    for (int i = 1; i < t.lines.Count; i += 2) {
+      var start = t.lines[i - 1];
+      var end = t.lines[i];
+
+      EditorGUI.BeginChangeCheck();
+      if (shift) {
+        // Creating new lines from first line works but not from other lines
+        if (blockCreate) {
+          if (i == createdAt) {
+            Vector3 newStart = Handles.PositionHandle(start, Quaternion.identity);
+            Vector3 newEnd = Handles.PositionHandle(end, Quaternion.identity);
+            if (EditorGUI.EndChangeCheck()) {
+              Undo.RecordObject(t, "Change line position");
+              t.lines[i - 1] = newStart;
+              t.lines[i] = newEnd;
+            }
+          }
+        } else {
+          Vector3 newStart = Handles.PositionHandle(start, Quaternion.identity);
+          Vector3 newEnd = Handles.PositionHandle(end, Quaternion.identity);
+          if (EditorGUI.EndChangeCheck()) {
+            Undo.RecordObject(t, "Added new line");
+            if (newStart == start) {
+              t.lines.Add(newEnd);
+              t.lines.Add(newEnd);
+            } else {
+              t.lines.Add(newStart);
+              t.lines.Add(newStart);
+            }
+            createdAt = t.lines.Count - 1;
+            blockCreate = true;
+            if (!Application.isPlaying) {
+              EditorUtility.SetDirty(t);
+              UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(UnityEditor.SceneManagement.EditorSceneManager.GetActiveScene());
+            };
+          }
+        }
+      } else {
+        Vector3 newStart = Handles.PositionHandle(start, Quaternion.identity);
+        Vector3 newEnd = Handles.PositionHandle(end, Quaternion.identity);
+        if (EditorGUI.EndChangeCheck()) {
+          Undo.RecordObject(t, "Change line position");
+          t.lines[i - 1] = newStart;
+          t.lines[i] = newEnd;
+        }
+      }
+    }
+  }
+}
