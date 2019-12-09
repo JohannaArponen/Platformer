a62293d4dc8907b046208aefda59cca3f6a560dc
Samuel <32210043+Satsaa@users.noreply.github.com>
2019-12-10 00:24:35 +0200

    Reflection physics

--- PlatformerProject/Assets/Scripts/Physics/Physics2DBouncyMove.cs
+++ PlatformerProject/Assets/Scripts/Physics/Physics2DBouncyMove.cs
@@ -1,17 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Mathematics;
+
 
 [RequireComponent(typeof(Collider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
 public class Physics2DBouncyMove : MonoBehaviour {
+
+  public float2 velocity;
+  [Tooltip("Layers which are checked by the raycasts")]
+  public ContactFilter2D layers;
+  [Range(0, 90)]
+  [Tooltip("Minimum collision angle for reflection. Smaller collision angles make the object slide along the collision")]
+  public float minAngle;
+  [Range(0, 90)]
+  [Tooltip("Maximum collision angle for reflection. Bigger collision angles stop the object")]
+  public float maxAngle;
+  [Tooltip("Avoid getting stuck inside colliders by offsetting collision positions")]
+  public float contactOffset = 0.003771f;
+
+
+  private Physics2DCastUtil cast;
+  private Collider2D col;
+  private Rigidbody2D rb;
+
   // Start is called before the first frame update
   void Start() {
-
+    col = GetComponent<BoxCollider2D>();
+    rb = GetComponent<Rigidbody2D>();
   }
 
   // Update is called once per frame
   void Update() {
+    if (cast == null) cast = new Physics2DCastUtil(transform, rb, layers);
+    var endVel = velocity * Time.deltaTime;
 
+    for (int i = 0; i < 10; i++) {
+      var hit = cast.Cast(transform.position, endVel);
+      if (hit) {
+        var collisionPos = CollisionPos(hit, transform.position, endVel);
+        cast.TryMoveTo(collisionPos);
+        velocity = Vector2.Reflect(velocity, hit.normal);
+        endVel *= 1 - hit.fraction;
+        if (math.lengthsq(endVel) < 0.001f) {
+          break;
+        }
+      } else {
+        cast.TryMoveTo(transform.position.AddXY(endVel));
+        break;
+      }
+    }
+  }
+
+  public Vector2 CollisionPos(RaycastHit2D hit, Vector2 origin, Vector2 dir) {
+    if (hit.distance <= contactOffset) return origin;
+    return origin + dir.SetLen(hit.distance - contactOffset);
   }
 }
---
+++ PlatformerProject/Assets/Scripts/Physics/Physics2DCastUtil.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physics2DCastUtil {
+
+  public Transform transform;
+  public Rigidbody2D rb;
+  public ContactFilter2D layers;
+
+  public Physics2DCastUtil(Transform transform, Rigidbody2D rb, ContactFilter2D layers) {
+    this.transform = transform;
+    this.rb = rb;
+    this.layers = layers;
+  }
+
+  public RaycastHit2D Collides(Vector2 pos) => Cast(pos, Vector2.zero);
+
+  public bool TryMoveTo(Vector2 pos) {
+    var cast = Cast(pos, Vector2.zero);
+    if (cast)
+      return false;
+    transform.position = pos;
+    return true;
+  }
+
+  public RaycastHit2D Cast(Vector2 start, Vector2 dir) {
+    Vector2 normalized = dir.x == 0 && dir.y == 0 ? Vector2.right : dir.normalized;
+    float length = dir.magnitude;
+    var results = new RaycastHit2D[1];
+    var prevPos = transform.position;
+    transform.position = start;
+    Physics2D.SyncTransforms();
+    rb.Cast(normalized, layers, results, length);
+    transform.position = prevPos;
+    Physics2D.SyncTransforms();
+    return results[0];
+  }
+}
